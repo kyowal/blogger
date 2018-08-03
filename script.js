efd586ec0f121bc8befa9b213346505d91a40c17
Rahul Siwal <rahulsiwal62@gmail.com>
2018-08-03 11:39:32 +0530

    js and xml updated

--- script.js
+++ script.js
@@ -1,3 +1,127 @@
 $(document).ready(function(){
+var app = {};
+(function(_self) { 
+    _self.validateMobile = function(phone) {
+		var re = /\d{5}([- ]*)\d{6}/;
+		return re.test(String(phone).toLowerCase());
+    };
+	
+	_self.validateEmail = function(email) {
+		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(String(email).toLowerCase());
+    };
+	
+	_self.push = function(action, label, value, object, eventname) {
+		action 	= (typeof action !== 'undefined') ? action : false;
+		object 	= (typeof object !== 'undefined') ? object : 'Not Available';
+		label 	= (typeof label !== 'undefined') ? label : 'Not Available';
+		value 	= (typeof value !== 'undefined') ? value : 'Not Available';
+		debug("Analytics Call: Action: " + action + " :: Object: " + object + " :: Event: " + eventname + " :: Label : " + label + " :: Value: " + value);
+		switch(action){
+			case 'pageview':
+				ga('send', 'pageview', location.pathname);
+			break;
+			case 'event':
+				ga('send', 'event', object, eventname, label, value);
+			break;
+			case 'leftswipe':
+				ga('send', 'event', 'Page Swipe', 'Left Swipe', 'Swipe Left - ' + label, 1);
+			break;
+			case 'rightswipe':
+				ga('send', 'event', 'Page Swipe', 'Right Swipe', 'Swipe Right - ' + label, 1);
+			break;
+			case 'readmore':
+				ga('send', 'event', 'Read More', 'Read More Click', 'Read More - ' + label, 1);
+			break;
+			case 'wpshare':
+				ga('send', 'event', 'Soical Share', 'Whatsapp Share', 'Whatsapp Share - ' + label, 1);
+			break;
+			case 'fbshare':
+				ga('send', 'event', 'Soical Share', 'Facebook Share', 'Facebook Share - ' + label, 1);
+			break;
+			case 'openrelated':
+				ga('send', 'event', 'Open Article', 'Open Related Article', 'Open Article - ' + label, 1);
+			break;
+			case 'homepage':
+				ga('send', 'event', 'User Landing', 'Page Landing', 'Landing on Homepage', 1);
+			break;
+			case 'singlepage':
+				ga('send', 'event', 'User Landing', 'Page Landing', 'Landing on Article Page', 1);
+			break;
+			case 'postreading':
+				ga('send', 'event', 'Post Reading', 'Single Post Reading', 'Single Post Read - ' + label, 1);
+			break;
+			/** Record User Skip the subscription ***/
+			case 'skipsubscribe':
+				ga('send', 'event', 'Subscription Popup', 'Email Subscription', 'Skip Email Subscription', 1);
+			break;
+			case 'wpskipsubscribe':
+				ga('send', 'event', 'Subscription Popup', 'Whatsapp Subscription', 'Skip Wp Subscription', 1);
+			break;
+			/** Record User subscribed our service ***/
+			case 'emailsubscribe':
+				ga('send', 'event', 'Subscription Popup', 'Email Subscription', 'Email Subscribed', 1);
+			break;
+			case 'wpsubscribe':
+				ga('send', 'event', 'Subscription Popup', 'Whatsapp Subscription', 'Wp Subscribed', 1);
+			break;
+			case 'advertisement':
+				ga('send', 'event', 'Advertisement', label, value, 1);
+			break;
+			case 'skipads':
+				ga('send', 'event', 'Skip Advertisement', label, value, 1);
+			break;
+		}
+    };
+	
+	_self.loadMore = function($url, $isnext) {
+		$isnext = ( $isnext == true )? true : false;
+		$.ajax({
+			dataType: "html",
+			url: $url,
+			success: function($response){
+					if($isnext == true){
+						$('#nextload').html($($response).find('._topcouter ul').html());
+						$nextUrl 		= $($response).find('a.blog-pager-older-link').attr('href');
+						$("#nextload").data('newurl', $nextUrl);
+					}else{
+						$('#previousload').html($($response).find('._topcouter ul').html());
+						$previousUrl 	= $($response).find('a.blog-pager-newer-link').attr('href');
+					}
 
+					if($addView == true){
+						$addView = false;
+						$nextData = $("#nextload").html();
+						if($nextData != ""){
+							$("#currentload").html($nextData); // Load new posts in current view
+							$("#nextload").html(""); // Make Clear Next Load
+							initFixes(); // Call To Init for reintialize
+						}else{
+							loading(true);
+						}
+					}
+			}
+		});
+    };
+	
+	_self.l = function() {
+		
+    };
+	
+	_self.l = function() {
+		
+    };
+	
+	_self.l = function() {
+		
+    };
+	
+	_self.l = function() {
+		
+    };
+	
+	
+
+	
+})(app);
 });
